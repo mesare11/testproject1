ac99c02a45d742e588e2bd4df1eb19c837adae84
mesare11 <60405062+mesare11@users.noreply.github.com>
2020-01-29 12:34:40 +0200

    Update 1.py

--- 1.py
+++ 1.py
@@ -4,5 +4,5 @@ days=0
 while (currentInfected<=humanPopulation):
 currentInfected=currentInfected+currentInfected/2
 days+=1 
-print(format((currentInfected)) + ' людей поражено в день ' + str(days))
+print(format((currentInfected)) + ' людей поражено в день ' + format(days))
 print('Заражены все люди. На ' + format(days) + ' день наступил конец света')

27b8ea116bd42163e881de6c351c682d13ce0dee
mesare11 <60405062+mesare11@users.noreply.github.com>
2020-01-29 10:56:57 +0200

    Delete 2.py

--- 2.py
+++
@@ -1,8 +0,0 @@
-i=2000
-mas=['2000']
-while (i<=7444443000):
-    i=i+i//2 
-    mas.append(i)
-else:
-    print(mas)
-    print('Заражены все люди. На ' + str(len(mas)) + ' день наступил конец света')

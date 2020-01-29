1833937b93758c6cfac8cf45846267c2d53594d3
mesare11 <60405062+mesare11@users.noreply.github.com>
2020-01-29 12:07:04 +0200

    Update 2.py

--- 2.py
+++ 2.py
@@ -2,8 +2,7 @@ humanPopulation=7444443000
 currentInfected=2000
 allInfected=[currentInfected]
 while (currentInfected<=humanPopulation):
-    currentInfected=currentInfected+currentInfected//2 
-    allInfected.append(currentInfected)
-else:
-    print(allInfected)
-    print('Заражены все люди. На ' + format(len(allInfected)) + ' день наступил конец света')
+currentInfected=currentInfected+currentInfected//2 
+allInfected.append(currentInfected)
+print(allInfected)
+print('Заражены все люди. На ' + format(len(allInfected)) + ' день наступил конец света')
